c02377a934dc68f012671e54605234e45c941049
Mikesh Khanal <44762372+mkhanal1@users.noreply.github.com>
2019-05-07 12:49:55 +0530

    Add files via upload

---
+++ Powershell/Cloudview_Connector_Azure.ps1
@@ -0,0 +1,114 @@
+﻿$config = Get-Content ./config.json | ConvertFrom-Json
+$Username = $config.defaults.username
+$Password = $config.defaults.password
+$URL = $config.defaults.baseurl
+$debug = $config.defaults.debug
+$subscriptions = $config.defaults.subscriptions
+$directoryId = $config.defaults.directoryId
+$applicationId = $config.defaults.applicationId
+$authenticationKey = $config.defaults.authenticationKey
+
+function getSubscriptions($subscriptions)
+{
+	if (([IO.Path]::GetExtension($subscriptions)) -eq ".csv")
+	{
+		$file = Import-Csv -Path $subscriptions
+		$file | ForEach-Object {
+			AddConnector $_.subscriptionId
+		}
+	}
+	else
+	{
+		if ( $subscriptions -eq "all")
+		{
+			$subscriptions = $directoryId
+		}
+		$subscriptionId = Get-AzManagementGroup -GroupName $subscriptions -Expand -Recurse
+		$i = 0
+		While ($subscriptionId.Children[$i] -ne $null)
+		{
+			if ($subscriptionId.Children[$i].Type -eq "/subscriptions")
+			{
+				$subscriptionIds = $subscriptionId.Children[$i].Name
+				AddConnector $subscriptionIds
+				$i += 1
+			}
+			else
+			{
+				getSubscriptions $subscriptionId.Children[$i].Name
+				$i += 1
+			}
+		}
+	}
+}
+
+function main()
+{
+	$subscriptionIds = @()
+	
+	if (($Username -eq $null) -OR ($Password -eq $null) -OR ($URL -eq $null) -OR ($subscriptions -eq $null) -OR ($directoryId -eq $null) -OR ($applicationId -eq $null) -OR ($authenticationKey -eq $null))
+	{
+		write-host "Config information in ./config.json is not configured correctly. Exiting..."
+		break
+	}
+	else
+	{
+		$URI = $URL + "/cloudview-api/rest/v1/azure/connectors"
+		$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username,$Password)))
+
+		$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+		$headers.Add("X-Requested-With","Qualys Runbook (Powershell)")
+		$headers.Add("Accept","application/json")
+		$headers.Add("Content-Type","application/json")
+		$headers.Add("Authorization",("Basic {0}" -f $base64AuthInfo))
+
+		if ($debug -ne $null)
+		{
+			$debugfile = New-Item -itemType File -Name ("debug-" + $((Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")) + ".log")
+		}
+		getSubscriptions $subscriptions
+	}
+}
+
+function AddConnector($subscriptionIds)
+{
+	echo "------------------------------Creating AZURE Connectors--------------------------------"
+	Add-Content "------------------------------Creating AZURE Connectors--------------------------------" -Path $debugfile.Name
+	$bodycontent = @{
+	   "applicationId" = $applicationId;
+	   "authenticationKey" = $authenticationKey;
+	   "description" = $subscriptionIds;
+	   "directoryId"= $directoryId;
+	   "name" = $subscriptionIds;
+	   "subscriptionId" = $subscriptionIds;
+	}
+	$body = $bodycontent | ConvertTo-Json
+		
+	try
+	{
+		echo "Connector with below details is being created"
+		echo $body
+		Add-Content "Connector with below details" -Path $debugfile.Name
+		Add-Content $body -Path $debugfile.Name
+		$result = Invoke-RestMethod -Headers $headers -Uri $URI -Method Post -Body $body -SkipCertificateCheck -SslProtocol Tls12  
+		Write-Host "StatusCode" $result.StatusCode
+		if ($debug -ne $null)
+		{
+			Add-Content  -Path $debugfile.Name "is created Successfully"
+			Add-Content  -Path $debugfile.Name $result.content
+			Add-Content  -Path $debugfile.Name "-------------------------------------------------------------------------"
+		}
+	}
+	catch
+	{
+		Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+		Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+		if ($debug -ne $null)
+		{
+			Add-Content  -Path $debugfile.Name "is not created Successfully"
+			Add-Content  -Path $debugfile.Name $_.Exception
+			Add-Content  -Path $debugfile.Name "-------------------------------------------------------------------------"
+		}
+	}
+}		
+main
